6cac5dc9ef6758f8e280f1b103ef808a082653ab
marti <marti@192.168.1.8>
2018-08-17 22:29:55 -0600

    estoy trabajando en mi tarea, voy por la mitad del menu :)

--- src/menutarea3/Menutarea3.java
+++ src/menutarea3/Menutarea3.java
@@ -5,6 +5,8 @@
  */
 package menutarea3;
 
+import java.util.Scanner;
+
 /**
  *
  * @author marti
@@ -16,6 +18,48 @@ public class Menutarea3 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    }
+        int op=0;
+        while(op<5){
+           int opcion;// para seleccionar una opcion
+           System.err.println("MENU");// menu de opciones
+           System.out.println("1.USUARIOS");
+           System.out.println("2.CONTADOR DE DIGITOS REPETIDOS");
+           System.out.println("3.TRES NUMEROS DE MAYOR A MENOR");
+           System.out.println("4.CALCULAR PROMEDIO");
+           System.out.println("5.SALIR");
+           System.out.println("INGRESE NUMERO DE OPCION:");
+       
+          Scanner a = new Scanner (System.in);
+          opcion= a.nextInt();
+        
+        // el do es para que se repita el menu 
+         
+           if(opcion==1){
+           USUARIOS obj=new USUARIOS();
+            obj.menuusuarios(); // para llamar al otro menu
+           }
+        
+           if(opcion==2){
+            numerosrepetidos obj=new numerosrepetidos();
+            obj.numr(); // para llamar al otro menu
+           }
+        
+          if(opcion==3){
+           MAYORAMENOR obj=new MAYORAMENOR();
+            obj.maramer(); // para llamar al otro menu 
+          }
+        
+          if(opcion==4){
+            PROMEDIO obj=new PROMEDIO();
+            obj.promediousers(); // para llamar al otro menu
+          }
+        
+         if(opcion==5){
+            System.err.println("GRACIAS POR UTILIZAR EL MENU :)");
+         }
+        opcion=op;
+      };// el ciclo se repite si las opciones entre 0 y 4, 5 sale
+        
     
+  } 
 }
